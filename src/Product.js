8afadcff590faef4581bbd38ddaa44798e07b118
Kiwi <hallerlucas@outlook.com>
2021-01-14 17:13:49 +0100

    Update

---
+++ src/Product.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Product = () => {
+    return (
+        <div className="product">
+            <h1>Master Widget</h1>
+            <p>price $125</p>
+        </div>
+    );
+}
+
+export default Product;
